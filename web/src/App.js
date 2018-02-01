0573457275c0930c9cc607b1d12bc699998cbbcb
Ben Pingilley <ben.pingilley@gmail.com>
2018-02-01 14:27:03 -0700

    Basic UI functionality added. Fields for user info appear if user is not found.

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Form from './Form.js';
 
@@ -7,9 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
         <Form />
       </div>
     );
